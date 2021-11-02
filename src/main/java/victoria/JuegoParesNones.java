d59d342ca4c5c051e901e8a4ba071a9bba0b21bf
Victoria Sampalo Garcia <vikytasampalo@gmail.com>
2021-11-02 13:09:19 +0100

    Se han añadido ambas opciones del menú

--- src/main/java/victoria/JuegoParesNones.java
+++ src/main/java/victoria/JuegoParesNones.java
@@ -20,7 +20,7 @@ public class JuegoParesNones {
         
         //DEFINO VARIABLES A USAR---------------------------
         
-        String opcion = null;
+        String opcion;
         
         int NumPares, NumNones;
         
@@ -28,7 +28,7 @@ public class JuegoParesNones {
         
         //constantes
         final int MAX_VALUE = 10; 
-        final int MIN_VALUE = 1;
+        final int MIN_VALUE = 0;
         
         //-----------------MENU--------------------------
         do { //repetir el bucle hasta que yo no le diga salir.
@@ -40,13 +40,34 @@ public class JuegoParesNones {
                System.out.println("1. Modalidad Manual");
                System.out.println("2. Modalidad juega con la máquina");
                System.out.println("3. Salir dle juego ");
-
+               System.out.println("Ha elegido la opción: ");
+               
+               //pido dato al user de su opcion
+               opcion = teclado.nextLine();
             } //end do2
-           while (opcion == "1" || opcion == "2");
-
-            
-
-            //realizar las operaciones  
+           while (!opcion.equals("1") && !opcion.equals("2") );
+            //estructura de while en string (!opcion.equals() &&...);
+         
+           switch (opcion){
+             
+               case "1": 
+                   System.out.println("1. MODALIDAD MANUAL\n-----------\n");
+                   
+                   
+                break;
+          
+           
+               case "2":
+                   System.out.println("2.MODALIDAD CONTRA LA MAQUINA\n-----------\n");
+                   
+                break;
+                
+               default:
+                break;
+                
+                   
+           }//end witch
+       
         }//end do1
         while (!opcion.equals("3"));
         
